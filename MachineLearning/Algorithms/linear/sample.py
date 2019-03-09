ea7418e126e3263dde2ecb06ebee015e98524565
Akhilez <akhild18@yahoo.com>
2019-03-09 16:22:32 +0530

    linear

--- MachineLearning/Algorithms/linear/sample.py
+++ MachineLearning/Algorithms/linear/sample.py
@@ -1,12 +1,12 @@
-#Import Library
-#Import other necessary libraries like pandas, numpy...
 from sklearn import linear_model
+import matplotlib.pylab as plt
+import numpy as np
 
-#Load Train and Test datasets
-#Identify feature and response variable(s) and values must be numeric and numpy arrays
-x_train=input_variables_values_training_datasets
-y_train=target_variables_values_training_datasets
-x_test=input_variables_values_test_datasets
+# equation => y = 3x - 6
+
+x_train = np.array([0, 1, 2, 3, 4, 5, 6])
+y_train = np.array([-6, -3, 0, 3, 6, 9, 12])
+x_test = np.array([-1, 3, 7, 14, -7, -14, 2.5])
 
 # Create linear regression object
 linear = linear_model.LinearRegression()
@@ -15,9 +15,10 @@ linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
 linear.score(x_train, y_train)
 
-#Equation coefficient and Intercept
+# Equation coefficient and Intercept
 print('Coefficient: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
 
-#Predict Output
-predicted= linear.predict(x_test)
+# Predict Output
+#predicted = linear.predict(x_test)
+#print(predicted)
