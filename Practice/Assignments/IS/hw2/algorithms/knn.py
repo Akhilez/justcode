807e38f9f24a82e22807ce603d2f9c5e09ce097b
Akhilez <akhild18@yahoo.com>
2019-10-04 18:12:21 -0400

    Working on KNN

--- Practice/Assignments/IS/hw2/algorithms/knn.py
+++ Practice/Assignments/IS/hw2/algorithms/knn.py
@@ -25,10 +25,8 @@ class KNearestNeighbours:
         return hits/len(predicted)
 
     def get_closest_neighbouring_classes(self, x):
-        neighbours = []
-        classes = []
-        # TODO: Find k closest neighbours to x
-        # TODO: Get the class of the neighbour
+        k_neighbours_indices = self.get_k_neighbours_indices(x)
+        classes = [self.y[i] for i in k_neighbours_indices]
         return classes
 
     def get_best_class(self, neighbours):
@@ -46,3 +44,26 @@ class KNearestNeighbours:
                 counts[neighbour] = count
 
         return max(counts.values())
+
+    def get_k_neighbours_indices(self, x):
+        # TODO: Find k closest neighbours to x
+
+        # Find the standard deviation of the data.
+        standard_deviations = self.get_standard_deviations()
+
+        # From x, create a radius of x - std and x + std
+        x_range = range(x[0])
+
+        # Find all the points in this radius. If these points are more than k, stop.
+        # Else, radius += std. Find all now.
+        # Find the distances of all the points in this radius.
+        # Pick the shortest k distances and return their indices
+
+    def get_standard_deviations(self):
+        # Find std for each attribute
+
+        # avg_x = sum(self.x)
+        # std_x = (sum([(avg_x - i) ** 2 for i in self.x]) / len(self.x)) ** 0.5
+
+        # return std_x
+        pass
