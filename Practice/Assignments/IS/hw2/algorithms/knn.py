8bf04eeee9f88ff1183474d05a0ffd1a2e4095fe
Akhilez <akhild18@yahoo.com>
2019-10-04 19:23:32 -0400

    Working on KNN

--- Practice/Assignments/IS/hw2/algorithms/knn.py
+++ Practice/Assignments/IS/hw2/algorithms/knn.py
@@ -22,7 +22,7 @@ class KNearestNeighbours:
         for i in range(len(predicted)):
             if predicted[i] == actual[i]:
                 hits += 1
-        return hits/len(predicted)
+        return hits / len(predicted)
 
     def get_closest_neighbouring_classes(self, x):
         k_neighbours_indices = self.get_k_neighbours_indices(x)
@@ -46,24 +46,58 @@ class KNearestNeighbours:
         return max(counts.values())
 
     def get_k_neighbours_indices(self, x):
-        # TODO: Find k closest neighbours to x
 
         # Find the standard deviation of the data.
         standard_deviations = self.get_standard_deviations()
 
-        # From x, create a radius of x - std and x + std
-        x_range = range(x[0])
+        radius = 1
 
-        # Find all the points in this radius. If these points are more than k, stop.
-        # Else, radius += std. Find all now.
-        # Find the distances of all the points in this radius.
+        while True:
+
+            # From x, create a radius of x - std and x + std
+            ranges = [range(x[i] - radius * standard_deviations, x[i] + radius * standard_deviations) for i in
+                      range(len(standard_deviations))]
+
+            # Find all the points in this radius.
+            points_in_ranges_indices = [i for i in range(len(self.x)) if
+                                all([int(self.x[i][j]) in ranges[j] for j in range(len(ranges))])]
+
+            # If these points are more than k, stop.
+            if len(points_in_ranges_indices) > self.k:
+                break
+            # Else, radius += std. Find all now.
+            else:
+                radius += 1
+
+        distances = [self.find_distance(x, self.x[i]) for i in points_in_ranges_indices]
+
         # Pick the shortest k distances and return their indices
+        min_distance_indices = []
+        distances_copy = list(distances)
+        for i in range(self.k - 1):
+            min_distance_index = distances_copy.index(min(distances_copy))
+            min_distance_indices.append(min_distance_index)
+            del distances_copy[min_distance_index]
+            i += 1
+
+        return [points_in_ranges_indices[i] for i in min_distance_indices]
 
     def get_standard_deviations(self):
         # Find std for each attribute
 
-        # avg_x = sum(self.x)
-        # std_x = (sum([(avg_x - i) ** 2 for i in self.x]) / len(self.x)) ** 0.5
+        if len(self.x) == 0:
+            return
 
-        # return std_x
-        pass
+        stds = []
+
+        for i in range(self.x[0]):
+            column = [j[i] for j in self.x]
+            avg = sum(column)
+            std = (sum([(avg - i) ** 2 for i in column]) / len(column)) ** 0.5
+            stds.append(std)
+
+        return stds
+
+    def find_distance(self, x1, x2):
+        # TODO: Find euclidian distance between these two points.
+        return 0
