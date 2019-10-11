3f44defa19a6d8037551aa45fa0a08e3e0622a62
Akhilez <akhild18@yahoo.com>
2019-10-11 00:17:43 -0400

    Added visualization to look into the problem data

--- Practice/Assignments/IS/hw2/visualization.py
+++ Practice/Assignments/IS/hw2/visualization.py
@@ -1,4 +1,5 @@
 from data_manager import DataManager
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -6,7 +7,13 @@ def main():
     data = data_manager.get_data()
     data = data_manager.get_rescaled_data(data, data_manager.get_scaling_function(data))
 
+    ones = [x[:2] for x in data if x[2] == 1]
+    zeros = [x[:2] for x in data if x[2] == 0]
 
+    plt.scatter([x[0] for x in ones], [x[1] for x in ones])
+    plt.scatter([x[0] for x in zeros], [x[1] for x in zeros])
+
+    plt.show()
 
 
 if __name__ == "__main__":
