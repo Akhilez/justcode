46ab5e766f60d349ba9ce2540dc7e36443b448b4
Akhilez <akhild18@yahoo.com>
2019-11-01 00:05:28 -0400

    Stated working on neural networks

---
+++ Practice/Assignments/IS/hw3/neural_network.py
@@ -0,0 +1,12 @@
+
+class Sequential:
+    def __init__(self):
+        self.layers = []
+
+    def add(self, layer):
+        self.layers.append(layer)
+
+
+class Dense:
+    def __init__(self):
+        pass
