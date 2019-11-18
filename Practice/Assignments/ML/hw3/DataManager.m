22dab2cca5d9595ced8068dc28f0dcd041bff4b0
Akhilez <akhild18@yahoo.com>
2019-11-18 13:27:48 -0500

    Added abstract methods to data manipulation for SVM

---
+++ Practice/Assignments/ML/hw3/DataManager.m
@@ -0,0 +1,21 @@
+classdef DataManager < handle
+   properties
+      x
+      y
+      x_train
+      y_train
+      x_test
+      y_test
+   end
+   methods
+
+      function none = split(self, x, y, percentage)
+
+      end
+
+      function x_scaled = scale(x, min, max)
+
+      end
+
+   end
+end
